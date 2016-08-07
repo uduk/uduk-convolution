7c2965c3f5443c16920e8fb1c222ae20a9d6f683
uduk <iherlambang@gmail.com>
2016-08-07 22:21:29 +0700

    Create envelopefollower.c

---
+++ embedd/envelopefollower.c
@@ -0,0 +1,29 @@
+double *
+envelopeFollower (double *originalSignal, long originalLen) 
+{
+  int sampleRate = 44100;
+  double attackTime =  0.0002;
+  double releaseTime =  0.0004;
+
+  float gainAttack = (float) exp(-1.0 / (sampleRate*attackTime));
+  float gainRelease = (float) exp(-1.0 / (sampleRate*releaseTime));
+  float envelopeOut = 0.0f;
+
+  double *envelopeBuffer = (double *) calloc (originalLen, sizeof (double));
+
+  for (int i = 0; i < originalLen; i++) {
+
+    float envelopeIn = fabs(originalSignal[i]);
+
+    if (envelopeOut < envelopeIn) {
+      envelopeOut = envelopeIn + gainAttack * (envelopeOut - envelopeIn);
+    } else {
+      envelopeOut = envelopeIn + gainRelease * (envelopeOut - envelopeIn);
+    }   
+
+    envelopeBuffer[i] = envelopeOut;
+  }
+
+  return envelopeBuffer;
+
+}
